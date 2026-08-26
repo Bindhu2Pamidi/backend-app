a412bb92f7acb22aa581574da7bedb0775b130bf
Bindhu2Pamidi <bindhup1026@gmail.com>
2026-08-26 15:43:52 -0500

    fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,12 +41,7 @@ resource "aws_security_group" "ecs"{
         protocol = "tcp"
         security_groups = [aws_security_group.alb.id]
     }
-    egress{
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    egress = []
 }
 resource "aws_security_group" "postgres" {
   name   = "${local.name}-postgres-sg"
