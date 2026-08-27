a1dd1bb92e3f41a5fa1fc6224d688d2aa0ead777
Bindhu2Pamidi <bindhup1026@gmail.com>
2026-08-27 17:47:28 -0500

    added5

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "database_endpoint" {
   value       = aws_db_instance.postgres.address
 }
 
-output "ecs_tasks_console_url" {
+output "application_url" {
   value       = "https://amazon.com{aws_ecs_cluster.app.name}/tasks?region=us-east-1"
 }
